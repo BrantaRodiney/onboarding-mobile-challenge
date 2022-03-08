f4853683501d674a309ad0bc57a974304aa53219
Rodiney Branta <branta.rodiney@gmail.com>
2022-03-08 17:25:33 -0300

    Extension added to make RootViewController as the main controller for all different section transitions

--- moedadigital/moedadigital/Controllers/RootViewController.swift
+++ moedadigital/moedadigital/Controllers/RootViewController.swift
@@ -7,8 +7,17 @@
 
 import UIKit
 
-class RootViewController: UIViewController {
+extension AppDelegate{
+    static var shared: AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
+    var rootViewController: RootViewController{
+        return window!.rootViewController as! RootViewController
+    }
+}
 
+class RootViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
