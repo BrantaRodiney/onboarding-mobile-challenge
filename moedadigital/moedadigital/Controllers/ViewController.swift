87203ef566f47398bd085e3ca091545454b728dc
Tatiana Pessoa Fernandes <tatianapeople@yahoo.com.br>
2022-03-08 16:01:17 -0300

    added tab-bar

--- moedadigital/moedadigital/Controllers/ViewController.swift
+++ moedadigital/moedadigital/Controllers/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // criar instância dos view controllers
+        let moedas = MoedasViewController()
+        let adicionadas = AdicionadasViewController()
+        
+        // definir titulos tab bar
+        moedas.title = "Moedas"
+        adicionadas.title = "Adicionadas"
+        
+        // atribuir view controllers à tab Bars
+        self.setViewControllers([moedas, adicionadas], animated: false)
+        
+        guard let items = self.tabBar.items else { return }
+        self.tabBar.tintColor = .gray
+        
+        let images = ["house.square", "star.fill"]
+        
+        for x in 0...1 {
+            items[x].image = UIImage(named: images[x])
+        }
+      
     }
 
+}
 
+class MoedasViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+    
+    }
+}
+
+class AdicionadasViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+    
+    }
 }
 
