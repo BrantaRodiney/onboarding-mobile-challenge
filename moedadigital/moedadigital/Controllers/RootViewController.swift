7b94b095afd49bbd7e485de16f803b350bd72bb6
Rodiney Branta <branta.rodiney@gmail.com>
2022-03-08 16:55:54 -0300

    Modified from ViewController to RootViewController

--- moedadigital/moedadigital/Controllers/RootViewController.swift
+++ moedadigital/moedadigital/Controllers/RootViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = .lightGray
         // Do any additional setup after loading the view.
     }
 
