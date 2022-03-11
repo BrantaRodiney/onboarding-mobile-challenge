d21756f2f15cf27bb309cb4cfae6eddf688f6a9b
Rodiney Branta <branta.rodiney@gmail.com>
2022-03-11 11:51:34 -0300

    Added the view class

---
+++ moedadigital/moedadigital/Controllers/CoinToFavoriteViewController.swift
@@ -0,0 +1,14 @@
+//
+//  coinToFavorite.swift
+//  moedadigital
+//
+//  Created by Rodiney Branta on 10/03/22.
+//
+
+import UIKit
+
+class CoinToFavoriteViewController: UIViewController {
+
+}
+
+
