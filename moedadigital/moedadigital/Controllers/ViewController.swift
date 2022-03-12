5573952d464aa00c59ab719fd05e1f7d3e79835b
Rodiney Branta <branta.rodiney@gmail.com>
2022-03-11 22:41:40 -0300

    removed deprecaetd code

--- moedadigital/moedadigital/Controllers/ViewController.swift
+++ moedadigital/moedadigital/Controllers/ViewController.swift
@@ -8,47 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let coinToFavoriteViewController = CoinToFavoriteViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        addCoinToFavoriteViewController()
-    }
-    
-    func addCoinToFavoriteViewController() {
-        addChild(coinToFavoriteViewController)
-        view.addSubview(coinToFavoriteViewController.view)
-        
-        coinToFavoriteViewController.view.backgroundColor = .darkGray
-        
-        coinToFavoriteViewController.didMove(toParent: self)
-        setCoinToFavoriteViewControllerConstraints()
-    }
-    
-    func setCoinToFavoriteViewControllerConstraints(){
-        coinToFavoriteViewController.view.translatesAutoresizingMaskIntoConstraints = false
-        coinToFavoriteViewController.view.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 20).isActive = true
-        coinToFavoriteViewController.view.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 20).isActive = true
-        coinToFavoriteViewController.view.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -20).isActive = true
-        coinToFavoriteViewController.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -245).isActive = true
-    }
-}
-
-@propertyWrapper
-public struct UseAutoLayout<T: UIView> {
-    public var wrappedValue: T {
-        didSet {
-            setAutoLayout()
-        }
-    }
-    
-    public init(wrappedValue: T) {
-        self.wrappedValue = wrappedValue
-        setAutoLayout()
-    }
-    
-    func setAutoLayout(){
-        wrappedValue.translatesAutoresizingMaskIntoConstraints = false
     }
 }
