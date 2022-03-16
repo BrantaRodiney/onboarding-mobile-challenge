90f2c2f9f1ebc03d4d0f82bbd2469d6e06ba4ff5
Tatiana Pessoa Fernandes <tatianapeople@yahoo.com.br>
2022-03-16 13:12:18 -0300

    Api Service function in App Delegate was called

---
+++ moedadigital/moedadigital/CryptoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CryptoTableViewCell.swift
+//  moedadigital
+//
+//  Created by Tatiana Pessoa Fernandes on 16/03/22.
+//
+
+import UIKit
+
+class CryptoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
