bf06f9ae09bb27b56b0aba9e2f43104faefc56b6
Rodiney Branta <branta.rodiney@gmail.com>
2022-03-17 11:47:23 -0300

    Implemented an extension to add a subtitle to the navigation bar using a StackView

--- moedadigital/moedadigital/Controllers/CoinsViewController.swift
+++ moedadigital/moedadigital/Controllers/CoinsViewController.swift
@@ -27,9 +27,42 @@ class CoinsViewController: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: true)
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         navigationController?.navigationBar.barTintColor = UIColor(red: 25/255.0, green: 24/255.0, blue: 25/255.0, alpha: 1)
-        navigationItem.title = "Moeda Digital"
-        navigationItem.title = "\(getTimeStampDateToString())"
+       // navigationItem.title = "Moeda Digital"
+        navigationItem.setTitle(title: "Moeda Digital", subtitle: "\(getTimeStampDateToString())")
+      //  navigationItem.title = "\(getTimeStampDateToString())"
         
     }
-    
+
+}
+
+extension UINavigationItem
+{
+    func setTitle(title:String, subtitle:String) {
+        
+        let main = UILabel()
+        main.text = title
+        main.font = UIFont.systemFont(ofSize: 17)
+        main.textColor = .white
+        main.sizeToFit()
+        
+        let date = UILabel()
+        date.text = subtitle
+        date.font = UIFont.systemFont(ofSize: 15)
+        date.textColor = .white
+        date.textAlignment = .center
+        date.sizeToFit()
+        
+        let stackView = UIStackView(arrangedSubviews: [main, date])
+        stackView.distribution = .equalCentering
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        
+        let width = max(main.frame.size.width, date.frame.size.width)
+        stackView.frame = CGRect(x: 0, y: 0, width: width, height: 40)
+        
+        date.sizeToFit()
+        main.sizeToFit()
+        
+        self.titleView = stackView
+    }
 }
