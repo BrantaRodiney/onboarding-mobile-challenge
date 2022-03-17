2928e9926fd54c1f261b9a916a3151025cf70e91
Rodiney Branta <branta.rodiney@gmail.com>
2022-03-17 05:06:15 -0300

    Implemented the basic layout configurarion setup for the CoinTabBar and Coins controllers

--- moedadigital/moedadigital/Controllers/CoinTabBarController.swift
+++ moedadigital/moedadigital/Controllers/CoinTabBarController.swift
@@ -9,8 +9,27 @@ import UIKit
 
 class CoinTabBarController: UITabBarController {
     
+    func tabBar()
+    {
+        let coinController = UINavigationController(rootViewController: CoinsViewController())
+        coinController.tabBarItem.image = UIImage(named: "dollar")
+        coinController.tabBarItem.selectedImage = UIImage(named: "dollar.fill")
+        coinController.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        
+        let favoriteController = UINavigationController(rootViewController: FavoriteCoinsViewController())
+        favoriteController.tabBarItem.image = UIImage(named: "star-five-points-shape")
+        favoriteController.tabBarItem.selectedImage = UIImage(named: "star-five-points-shape.fill")
+        favoriteController.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        
+        tabBar.barTintColor = .black
+        tabBar.tintColor = .yellow
+        
+        viewControllers = [coinController, favoriteController]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tabBar()
     }
     
 }
--- moedadigital/moedadigital/Controllers/CoinsViewController.swift
+++ moedadigital/moedadigital/Controllers/CoinsViewController.swift
@@ -8,9 +8,16 @@
 import UIKit
 
 class CoinsViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //view.backgroundColor = UIColor.black
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.barTintColor = UIColor(red: 25/255.0, green: 24/255.0, blue: 25/255.0, alpha: 1)
+        navigationItem.title = "Moeda Digital"
+        
     }
 
 }
