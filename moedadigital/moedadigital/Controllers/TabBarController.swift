4dfb137c04f457b75de556e2ac44c58b6d61eda4
Rodiney Branta <branta.rodiney@gmail.com>
2022-03-17 22:53:36 -0300

    Refactored the TabBarController

--- moedadigital/moedadigital/Controllers/TabBarController.swift
+++ moedadigital/moedadigital/Controllers/TabBarController.swift
@@ -10,29 +10,55 @@ import UIKit
 
 class TabBarController: UITabBarController {
     
+    func tabBar()
+    {
+        let coinController = UINavigationController(rootViewController: MoedasViewController())
+        coinController.tabBarItem.image = UIImage(named: "dollar")
+        coinController.tabBarItem.selectedImage = UIImage(named: "dollar.fill")
+        coinController.tabBarItem.title = "Moedas"
+        coinController.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        
+        let favoriteController = UINavigationController(rootViewController: AdicionadasViewController())
+        favoriteController.tabBarItem.image = UIImage(named: "star-five-points-shape")
+        favoriteController.tabBarItem.selectedImage = UIImage(named: "star-five-points-shape.fill")
+        favoriteController.tabBarItem.title = "Adicionadas"
+        favoriteController.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        
+        tabBar.barTintColor = .black
+        tabBar.tintColor = .yellow
+        
+        viewControllers = [coinController, favoriteController]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // criar instância dos view controllers
-        let moedas = MoedasViewController()
-        let adicionadas = AdicionadasViewController()
-        
-        // definir titulos tab bar
-        moedas.title = "Moedas"
-        adicionadas.title = "Adicionadas"
-        
-        // atribuir view controllers à tab Bars
-        self.setViewControllers([moedas, adicionadas], animated: false)
-        
-        guard let items = self.tabBar.items else { return }
-        self.tabBar.tintColor = .gray
-        
-        let images = ["house.square", "star.fill"]
-        
-        for x in 0...1 {
-            items[x].image = UIImage(named: images[x])
-        }
-      
+        tabBar()
     }
+    
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // criar instância dos view controllers
+//        let moedas = MoedasViewController()
+//        let adicionadas = AdicionadasViewController()
+//
+//        // definir titulos tab bar
+//        moedas.title = "Moedas"
+//        adicionadas.title = "Adicionadas"
+//
+//        // atribuir view controllers à tab Bars
+//        self.setViewControllers([moedas, adicionadas], animated: false)
+//
+//        guard let items = self.tabBar.items else { return }
+//        self.tabBar.tintColor = .gray
+//
+//        let images = ["house.square", "star.fill"]
+//
+//        for x in 0...1 {
+//            items[x].image = UIImage(named: images[x])
+//        }
+//
+//    }
 
 }
