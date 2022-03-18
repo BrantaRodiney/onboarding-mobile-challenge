550c2928c43c98d50181fa346a99065eaf476539
Rodiney Branta <branta.rodiney@gmail.com>
2022-03-17 22:57:20 -0300

    --amend

--- moedadigital/moedadigital/Controllers/TabBarController.swift
+++ moedadigital/moedadigital/Controllers/TabBarController.swift
@@ -34,31 +34,5 @@ class TabBarController: UITabBarController {
         super.viewDidLoad()
         tabBar()
     }
-    
-    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // criar instância dos view controllers
-//        let moedas = MoedasViewController()
-//        let adicionadas = AdicionadasViewController()
-//
-//        // definir titulos tab bar
-//        moedas.title = "Moedas"
-//        adicionadas.title = "Adicionadas"
-//
-//        // atribuir view controllers à tab Bars
-//        self.setViewControllers([moedas, adicionadas], animated: false)
-//
-//        guard let items = self.tabBar.items else { return }
-//        self.tabBar.tintColor = .gray
-//
-//        let images = ["house.square", "star.fill"]
-//
-//        for x in 0...1 {
-//            items[x].image = UIImage(named: images[x])
-//        }
-//
-//    }
 
 }
